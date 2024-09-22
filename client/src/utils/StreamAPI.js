989681fa930f18da73f6af6394b13db53eb492ea
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-09-22 17:50:13 -0600

    getSources api call.

--- client/src/utils/StreamAPI.js
+++ client/src/utils/StreamAPI.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export default {
-
+  getSources: function() {
+    return axios.get('https://api.watchmode.com/v1/sources/?types=sub&apiKey=' + process.env.REACT_APP_WATCHMODED_API_KEY);
+  },
 };
