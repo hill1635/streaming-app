7e08b6eb10740ed07f3beafd4d74c07b4f7d59fd
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-09-22 18:27:38 -0600

    Added unused routes.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./users');
-const placeRoutes = require('./places');
 
 router.use('/users', userRoutes);
-router.use('/places', placeRoutes);
 
 module.exports = router;
