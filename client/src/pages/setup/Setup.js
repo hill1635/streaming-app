9af7b806603895706fd862069090b82f977d7964
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-09-22 18:09:06 -0600

    Setup page skeleton.

---
+++ client/src/pages/setup/Setup.js
@@ -0,0 +1,16 @@
+import React from 'react;
+import StreamAPI from '../../utils/StreamAPI';
+
+function Setup() {
+  return (
+    <main>
+      <div className="wrapper">
+        <header>
+          <h1>Setup Page</h1>
+        </header>
+      </div>
+    </main>
+  );
+}
+
+export default Setup;
