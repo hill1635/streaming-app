bfda807b7675093c7dc6eca22d3cee16de368a23
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-10-05 22:16:59 -0600

    Added services.

--- client/src/pages/account/Account.js
+++ client/src/pages/account/Account.js
@@ -4,15 +4,19 @@ import EditBtn from '../../components/buttons/Edit';
 import SaveBtn from '../../components/buttons/Save';
 import CancelBtn from '../../components/buttons/Cancel';
 import StreamAPI from '../../utils/StreamAPI';
+import './Account.scss';
 
 function Account() {
 	const [ edit, setEdit ] = useState(false);
-	const [ user, setUser ] = useState({first: "", last: "", email: ""});
+	const [ user, setUser ] = useState({first: "", last: "", email: "", services: [], genres: []});
 	const [ userDraft, setUserDraft ] = useState(user);
 
 	useEffect(() => {
-		console.log("edit:", edit);
-	}, [edit]); 
+		StreamAPI.getSources().then((res) => {
+			var filteredSources = res.data.filter((source) => source.regions.includes("US")).slice(0, 10);
+			setUserDraft({...user, services: filteredSources});
+		});
+	}, []); 
 
 	const saveAccount = () => {
 		setUser(userDraft);
@@ -47,6 +51,17 @@ function Account() {
 				}
 			</div>
 			<h3>Services:</h3>
+			{edit &&
+				userDraft.services.map((service) => {
+				return (
+					<div className="serviceProvider" key={service.id}>
+						<input type="checkbox" />
+						<img src={service.logo_100px} alt={service.name} />
+						<h4>{service.name}</h4>
+					</div>
+				);
+			})}
+			<h3>Genres:</h3>
 			{!edit &&
 				<EditBtn submit={() => setEdit(true)}/>
 			}
