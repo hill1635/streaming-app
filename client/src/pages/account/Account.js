c91120c06ad4c33d6ccfd6a83bdc4a382b703eb8
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-10-05 21:56:18 -0600

    Added user and draftUser states with conditional input fields.

--- client/src/pages/account/Account.js
+++ client/src/pages/account/Account.js
@@ -8,10 +8,16 @@ import StreamAPI from '../../utils/StreamAPI';
 function Account() {
 	const [ edit, setEdit ] = useState(false);
 	const [ user, setUser ] = useState({first: "", last: "", email: ""});
+	const [ draftUser, setDraftUser ] = useState({first: "", last: "", email: ""});
 
 	useEffect(() => {
 		console.log("edit:", edit);
 	}, [edit]); 
+
+	const saveAccount = () => {
+		setUser(draftUser);
+		setEdit(false);
+	};
 
 	return (
 		<main>
@@ -23,21 +29,21 @@ function Account() {
 					<p>{user.first}</p>
 				}
 				{edit &&
-					<input type="text" placeholder={user.first}></input>
+					<input type="text" placeholder={user.first} onChange={(e) => setDraftUser({...draftUser, first: e.target.value})}></input>
 				}
 				<h3>Last:</h3>
 				{!edit &&
 					<p>{user.last}</p>
 				}
 				{edit &&
-					<input type="text" placeholder={user.last}></input>
+					<input type="text" placeholder={user.last} onChange={(e) => setDraftUser({...draftUser, last: e.target.value})}></input>
 				}
 				<h3>Email:</h3>
 				{!edit &&
 					<p>{user.email}</p>
 				}
 				{edit &&
-					<input type="text" placeholder={user.email}></input>
+					<input type="text" placeholder={user.email} onChange={(e) => setDraftUser({...draftUser, email: e.target.value})}></input>
 				}
 			</div>
 			<h3>Services:</h3>
@@ -46,7 +52,7 @@ function Account() {
 			}
 			{edit &&
 			<div>
-				<SaveBtn submit={() => setEdit(false)}/>
+				<SaveBtn submit={() => saveAccount()}/>
 				<CancelBtn submit={() => setEdit(false)}/>
 			</div>
 			}
