3f7009408cec9276a40e342babda31b45b6f8075
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-10-05 21:58:53 -0600

    Changed input value to draftUser.

--- client/src/pages/account/Account.js
+++ client/src/pages/account/Account.js
@@ -8,7 +8,7 @@ import StreamAPI from '../../utils/StreamAPI';
 function Account() {
 	const [ edit, setEdit ] = useState(false);
 	const [ user, setUser ] = useState({first: "", last: "", email: ""});
-	const [ draftUser, setDraftUser ] = useState({first: "", last: "", email: ""});
+	const [ draftUser, setDraftUser ] = useState(user);
 
 	useEffect(() => {
 		console.log("edit:", edit);
@@ -29,21 +29,21 @@ function Account() {
 					<p>{user.first}</p>
 				}
 				{edit &&
-					<input type="text" placeholder={user.first} onChange={(e) => setDraftUser({...draftUser, first: e.target.value})}></input>
+					<input type="text" value={draftUser.first} onChange={(e) => setDraftUser({...draftUser, first: e.target.value})}></input>
 				}
 				<h3>Last:</h3>
 				{!edit &&
 					<p>{user.last}</p>
 				}
 				{edit &&
-					<input type="text" placeholder={user.last} onChange={(e) => setDraftUser({...draftUser, last: e.target.value})}></input>
+					<input type="text" value={draftUser.last} onChange={(e) => setDraftUser({...draftUser, last: e.target.value})}></input>
 				}
 				<h3>Email:</h3>
 				{!edit &&
 					<p>{user.email}</p>
 				}
 				{edit &&
-					<input type="text" placeholder={user.email} onChange={(e) => setDraftUser({...draftUser, email: e.target.value})}></input>
+					<input type="text" value={draftUser.email} onChange={(e) => setDraftUser({...draftUser, email: e.target.value})}></input>
 				}
 			</div>
 			<h3>Services:</h3>
