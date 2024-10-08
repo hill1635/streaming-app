7bc4c1f0e883ce2fda617ef0a6307d86d27b119f
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-10-07 22:23:41 -0600

    Added get api route.

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const userController = require('../../controllers/userController');
 
 router.route('/').post(userController.create);
 
+router.route('/:id').get(userController.get);
+
 router.route('/:id').delete(userController.remove);
 
 router.route('/login').post(userController.login);
