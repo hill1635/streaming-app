7c88131ab0469d54f3414155b7a4da9015a39eb4
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-11-02 13:18:10 -0600

    Added stream api data to useRefs instead of states.

--- client/src/pages/account/Account.js
+++ client/src/pages/account/Account.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState, useContext, useRef } from 'react';
 import { UserContext } from '../../UserContext';
 import EditBtn from '../../components/buttons/Edit';
 import SaveBtn from '../../components/buttons/Save';
@@ -11,81 +11,90 @@ function Account(props) {
 	const [ edit, setEdit ] = useState(false);
 	const { user, setUser } = useContext(UserContext);
 	const [ userDraft, setUserDraft ] = useState(user);
-	const [ genres, setGenres ] = useState([]);
+	const servicesRef = useRef([]);
 	const [ services, setServices ] = useState([]);
+	const genresRef = useRef([]);
+	const [ genres, setGenres ] = useState([]);
 
-			// StreamAPI.getSources().then((res) => {
-			// 	var filteredSources = res.data.filter((source) => source.regions.includes("US")).slice(0, 10);
-			// 	setServices(filteredSources);
-			// });
+  useEffect(() => {
+    StreamAPI.getSources().then((res) => {
+			console.log("Fetching sources");
+			console.log("sources:", res.data);
+      servicesRef.current = res.data;
+    });
 
-			// StreamAPI.getGenres().then((res) => {
-			// 	setGenres(res.data);
-			// });
+    StreamAPI.getGenres().then((res) => {
+			console.log("Fetching genres");
+			console.log("genres:", res.data);
+      genresRef.current = res.data;
+    });
+
+		setUserDraft(user);
+  }, []);
 	
 	return (
-		// <main>
-		// 	<h1>Account Info</h1>
-		// 	<div>
-		// 		<h2>Personal Info</h2>
-		// 		<h3>Display Name:</h3>
-		// 		{!edit &&
-		// 			<p>{user.display}</p>
-		// 		}
-		// 		{edit &&
-		// 			<input type="text" value={userDraft.display} onChange={(e) => setUserDraft({...userDraft, first: e.target.value})}></input>
-		// 		}
-		// 		<h3>Email:</h3>
-		// 			<p>{user.email}</p>
-		// 	</div>
-		// 	<h3>Services:</h3>
-		// 	{edit &&
-		// 		services.map((service) => {
-		// 		return (
-		// 			<div className="serviceProvider" key={service.id}>
-		// 				<input type="checkbox" onChange={(e) => selectService(e.target.checked, service)}/>
-		// 				<img src={service.logo_100px} alt={service.name} />
-		// 				<h4>{service.name}</h4>
-		// 			</div>
-		// 		);
-		// 	})}
-		// 	{!edit && 
-		// 		user.services.map((service) => {
-		// 		return (
-		// 			<div className="service" key={service.id}>
-		// 				<img src={service.logo_100px} alt={service.name} />
-		// 				<h4>{service.name}</h4>
-		// 			</div>
-		// 		);
-		// 	})}
-		// 	<h3>Genres:</h3>
-		// 	{edit &&
-		// 		genres.map((genre) => {
-		// 		return (
-		// 			<div key={genre.id}>
-		// 				<input type="checkbox" onChange={(e) => selectGenre(e.target.checked, genre)}/>
-		// 				<h4>{genre.name}</h4>
-		// 			</div>
-		// 		);
-		// 	})}
-		// 	{!edit &&
-		// 		user.genres.map((genre) => {
-		// 		return (
-		// 			<div key={genre.id}>
-		// 				<h4>{genre.name}</h4>
-		// 			</div>
-		// 		);
-		// 	})}
-		// 	{!edit &&
-		// 		<EditBtn submit={() => setEdit(true)}/>
-		// 	}
-		// 	{edit &&
-		// 	<div>
-		// 		<SaveBtn submit={() => updateUser(userDraft)}/>
-		// 		<CancelBtn submit={() => setEdit(false)}/>
-		// 	</div>
-		// 	}
-		// </main>
+		<main>
+			<h1>Account Info</h1>
+			<div>
+				<h2>Personal Info</h2>
+				<h3>Display Name:</h3>
+				{!edit &&
+					<p>{user.displayName}</p>
+				}
+				{edit &&
+					<input type="text" value={userDraft.displayName} onChange={(e) => setUserDraft({...userDraft, first: e.target.value})}></input>
+				}
+				<h3>Email:</h3>
+					<p>{user.email}</p>
+			</div>
+			<h3>Services:</h3>
+			{edit && servicesRef.current.length > 0 &&
+				servicesRef.current.map((service) => {
+				return (
+					<div className="serviceProvider" key={service.id}>
+						<input type="checkbox" checked={services.includes(service)}/>
+						<img src={service.logo_100px} alt={service.name} />
+						<h4>{service.name}</h4>
+					</div>
+				);
+			})}
+			{!edit && services.length > 0 && 
+				user.services.map((service) => {
+				return (
+					<div className="service" key={service.id}>
+						<img src={service.logo_100px} alt={service.name} />
+						<h4>{service.name}</h4>
+					</div>
+				);
+			})}
+			<h3>Genres:</h3>
+			{edit && genresRef.current.length > 0 &&
+				genresRef.current.map((genre) => {
+				return (
+					<div key={genre.id}>
+						<input type="checkbox" checked={genres.includes(genre)}/>
+						<h4>{genre.name}</h4>
+					</div>
+				);
+			})}
+			{!edit && genres.length > 0 &&
+				user.genres.map((genre) => {
+				return (
+					<div key={genre.id}>
+						<h4>{genre.name}</h4>
+					</div>
+				);
+			})}
+			{!edit &&
+				<EditBtn submit={() => setEdit(true)}/>
+			}
+			{edit &&
+			<div>
+				<SaveBtn submit={() => console.log("save")}/>
+				<CancelBtn submit={() => setEdit(false)}/>
+			</div>
+			}
+		</main>
 	);
 }
 
