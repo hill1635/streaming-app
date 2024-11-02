b9c5face5fc3335f3dabcd5f50b0bfd3cc7f2a26
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-11-02 15:53:00 -0600

    Rearranged code.

--- client/src/UserContext.js
+++ client/src/UserContext.js
@@ -4,6 +4,7 @@ import UserAPI from './utils/UserAPI';
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
+  const init = useRef(false);
   const userRef = useRef({});
   const [user, setUser] = useState({
     email: "",
@@ -12,22 +13,24 @@ export const UserProvider = ({ children }) => {
     genres: [],
   });
 
-  const init = useRef(false);
+  const getUser = () => {
+    UserAPI.get()
+    .then((res) => {
+      const userObject = {
+        email: res.data?.email || "",
+        displayName: res.data?.displayName || "",
+        services: res.data?.services ? res.data.services.split(",") : [],
+        genres: res.data?.genres ? res.data.genres.split(",") : [],
+      };
+      setUser(userObject);
+    })
+    .catch((err) => {
+      console.error("Error fetching user data:", err);
+    });
+  };
 
   useEffect(() => {
-    UserAPI.get()
-      .then((res) => {
-        const userObject = {
-          email: res.data?.email || "",
-          displayName: res.data?.displayName || "",
-          services: res.data?.services ? res.data.services.split(",") : [],
-          genres: res.data?.genres ? res.data.genres.split(",") : [],
-        };
-        setUser(userObject);
-      })
-      .catch((err) => {
-        console.error("Error fetching user data:", err);
-      });
+    getUser();
   }, []);
 
   useEffect(() => {
