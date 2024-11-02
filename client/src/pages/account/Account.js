240f938e2e661e880d742380b0401c5a841f26c4
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-11-02 12:37:34 -0600

    Commented out component.

--- client/src/pages/account/Account.js
+++ client/src/pages/account/Account.js
@@ -24,68 +24,68 @@ function Account(props) {
 			// });
 	
 	return (
-		<main>
-			<h1>Account Info</h1>
-			<div>
-				<h2>Personal Info</h2>
-				<h3>Display Name:</h3>
-				{!edit &&
-					<p>{user.display}</p>
-				}
-				{edit &&
-					<input type="text" value={userDraft.display} onChange={(e) => setUserDraft({...userDraft, first: e.target.value})}></input>
-				}
-				<h3>Email:</h3>
-					<p>{user.email}</p>
-			</div>
-			<h3>Services:</h3>
-			{edit &&
-				services.map((service) => {
-				return (
-					<div className="serviceProvider" key={service.id}>
-						<input type="checkbox" onChange={(e) => selectService(e.target.checked, service)}/>
-						<img src={service.logo_100px} alt={service.name} />
-						<h4>{service.name}</h4>
-					</div>
-				);
-			})}
-			{!edit && 
-				user.services.map((service) => {
-				return (
-					<div className="service" key={service.id}>
-						<img src={service.logo_100px} alt={service.name} />
-						<h4>{service.name}</h4>
-					</div>
-				);
-			})}
-			<h3>Genres:</h3>
-			{edit &&
-				genres.map((genre) => {
-				return (
-					<div key={genre.id}>
-						<input type="checkbox" onChange={(e) => selectGenre(e.target.checked, genre)}/>
-						<h4>{genre.name}</h4>
-					</div>
-				);
-			})}
-			{!edit &&
-				user.genres.map((genre) => {
-				return (
-					<div key={genre.id}>
-						<h4>{genre.name}</h4>
-					</div>
-				);
-			})}
-			{!edit &&
-				<EditBtn submit={() => setEdit(true)}/>
-			}
-			{edit &&
-			<div>
-				<SaveBtn submit={() => updateUser(userDraft)}/>
-				<CancelBtn submit={() => setEdit(false)}/>
-			</div>
-			}
-		</main>
+		// <main>
+		// 	<h1>Account Info</h1>
+		// 	<div>
+		// 		<h2>Personal Info</h2>
+		// 		<h3>Display Name:</h3>
+		// 		{!edit &&
+		// 			<p>{user.display}</p>
+		// 		}
+		// 		{edit &&
+		// 			<input type="text" value={userDraft.display} onChange={(e) => setUserDraft({...userDraft, first: e.target.value})}></input>
+		// 		}
+		// 		<h3>Email:</h3>
+		// 			<p>{user.email}</p>
+		// 	</div>
+		// 	<h3>Services:</h3>
+		// 	{edit &&
+		// 		services.map((service) => {
+		// 		return (
+		// 			<div className="serviceProvider" key={service.id}>
+		// 				<input type="checkbox" onChange={(e) => selectService(e.target.checked, service)}/>
+		// 				<img src={service.logo_100px} alt={service.name} />
+		// 				<h4>{service.name}</h4>
+		// 			</div>
+		// 		);
+		// 	})}
+		// 	{!edit && 
+		// 		user.services.map((service) => {
+		// 		return (
+		// 			<div className="service" key={service.id}>
+		// 				<img src={service.logo_100px} alt={service.name} />
+		// 				<h4>{service.name}</h4>
+		// 			</div>
+		// 		);
+		// 	})}
+		// 	<h3>Genres:</h3>
+		// 	{edit &&
+		// 		genres.map((genre) => {
+		// 		return (
+		// 			<div key={genre.id}>
+		// 				<input type="checkbox" onChange={(e) => selectGenre(e.target.checked, genre)}/>
+		// 				<h4>{genre.name}</h4>
+		// 			</div>
+		// 		);
+		// 	})}
+		// 	{!edit &&
+		// 		user.genres.map((genre) => {
+		// 		return (
+		// 			<div key={genre.id}>
+		// 				<h4>{genre.name}</h4>
+		// 			</div>
+		// 		);
+		// 	})}
+		// 	{!edit &&
+		// 		<EditBtn submit={() => setEdit(true)}/>
+		// 	}
+		// 	{edit &&
+		// 	<div>
+		// 		<SaveBtn submit={() => updateUser(userDraft)}/>
+		// 		<CancelBtn submit={() => setEdit(false)}/>
+		// 	</div>
+		// 	}
+		// </main>
 	);
 }
 
