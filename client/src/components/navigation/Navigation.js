de77fdecd8045ab367666688ab90001e21128b3f
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-11-02 11:42:17 -0600

    Added conditional links to navigation based on user context.

--- client/src/components/navigation/Navigation.js
+++ client/src/components/navigation/Navigation.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { UserContext } from '../../UserContext';
 import './Navigation.scss';
 
-function Navigation(props) {
-	// return (
-	//   <nav className="nav" role="navigation">
-	//     {props.links.map((link) => (
-	//       <a href={link.href}>{link.name}</a>
-	//     ))}
-	//   </nav>
-	// );
+function Navigation() {
+	const { user } = useContext(UserContext);
+	const [ loggedIn, setLoggedIn ] = useState(false);
+
+	useEffect(() => {
+		setLoggedIn(Object.keys(user).length > 0);
+	}, [user]);
+
+	return (
+	  <nav className="nav" role="navigation">
+	    <a href="/">Home</a>
+			{!loggedIn &&
+				<span>
+					<a href="/login">Login</a>
+					<a href="/signup">Signup</a>
+				</span>
+			}
+			{loggedIn &&
+				<span>
+					<a href="/account">Account</a>
+					<a href="/">Logout</a>
+				</span>
+			}
+	  </nav>
+	);
 }
 
 export default Navigation;
