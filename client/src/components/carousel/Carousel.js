c14fa3ec4f18f70f4a2a7f191c1f08a79f4bf8c6
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-11-03 17:57:58 -0700

    Added carousel component.

---
+++ client/src/components/carousel/Carousel.js
@@ -0,0 +1,13 @@
+import React, { useContext, useState, useEffect } from 'react';
+import './Carousel.scss';
+
+function Carousel(props) {
+
+	return (
+    <section>
+      
+    </section>
+	);
+}
+
+export default Carousel;
