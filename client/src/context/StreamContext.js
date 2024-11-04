dd8176a3b4d7b1196ead8aff15e8d24977610cb1
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-11-03 18:30:40 -0700

    Added genres and sources states.

--- client/src/context/StreamContext.js
+++ client/src/context/StreamContext.js
@@ -3,8 +3,11 @@ import React, { createContext, useEffect, useState } from 'react';
 export const StreamContext = createContext();
 
 export const StreamProvider = ({ children }) => {
+  const [ sources, setSources ] = useState([]);
+  const [ genres, setGenres ] = useState([]);
+
   return (
-    <StreamContext.Provider value={{}}>
+    <StreamContext.Provider value={{ genres, sources }}>
       {children}
     </StreamContext.Provider>
   );
