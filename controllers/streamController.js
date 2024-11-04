4672f381ceeaebc164a23d9635eab8ed04fbb2b2
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-11-03 18:19:53 -0700

    Added function to query titles.

--- controllers/streamController.js
+++ controllers/streamController.js
@@ -20,5 +20,23 @@ module.exports = {
         console.error('Error getting genres:', err);
         res.status(500).json({ error: 'An error occurred while retrieving the genres' });
       });
-  }
+  },
+  getTitles: function (req, res) {
+    let params = "";
+    if (req.query.services) {
+      params += "&source_id=" + req.query.services;
+    }
+    if (req.query.genres) {
+      params += "&genres=" + req.query.genres;
+    }
+    axios.get('https://api.watchmode.com/v1/list-titles/?apiKey=' + process.env.REACT_APP_WATCHMODE_API_KEY + params)
+      .then((response) => {
+        res.json(response.data);
+      })
+      .catch((err) => {
+        console.error('Error getting titles:', err);
+        res.status(500).json({ error: 'An error occurred while retrieving the titles' });
+      }
+    );
+  },
 };
