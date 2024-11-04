6cdd1d917fd99c7071977529cb28d040ea5c78be
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-11-03 20:23:39 -0700

    Added StreamContext, getTitles function, and init filters with API data.

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,15 +1,53 @@
-import React, { useContext } from 'react';
-import { UserContext } from '../../UserContext';
+import React, { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../context/UserContext';
+import { StreamContext } from '../../context/StreamContext';
 import './Home.scss';
+import { streamFilters } from '../../utils/StreamFilters';
+import StreamAPI from '../../utils/StreamAPI';
 
 function Home() {
 	const { user } = useContext(UserContext);
+	const { sources, genres } = useContext(StreamContext);
+	const [ filters, setFilters ] = useState([]);
+
+	const getTitles = (params) => {
+		const serviceIds = params.services.map((service) => service.id).join(",");
+		const genreIds = params.genres.map((genre) => genre.id).join(",");
+		const searchParams = {
+			services: serviceIds,
+			genres: genreIds,
+		};
+		StreamAPI.getTitles(searchParams)
+			.then((res) => {
+				console.log("res:", res);
+			})
+			.catch((err) => {
+				console.error("Error fetching titles:", err);
+			});
+	};
+
+	const initFilters = () => {
+		const updatedFilters = streamFilters.map(filter => {
+			if (filter.key === "source_ids") {
+				return { ...filter, values: sources };
+			} else if (filter.key === "genres") {
+				return { ...filter, values: genres };
+			}
+			return filter;
+		});
+		setFilters(updatedFilters);
+	};
+	
+	useEffect(() => {
+		initFilters();
+	}, [ sources, genres ]);
 
 	return (
 		<main>
 			<div className="wrapper">
 				<h1>Welcome to the homepage!</h1>
 				<p>Get started here to for the first impression.</p>
+				<button onClick={() => getTitles({...user})}>Get Titles</button>
 			</div>
 		</main>
 	);
