70236d8938b0f5344ca160ec50e053235d3e790f
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-11-03 18:44:54 -0700

    Added API calls for sources and genres.

--- client/src/context/StreamContext.js
+++ client/src/context/StreamContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
+import StreamAPI from '../utils/StreamAPI';
 
 export const StreamContext = createContext();
 
@@ -6,9 +7,30 @@ export const StreamProvider = ({ children }) => {
   const [ sources, setSources ] = useState([]);
   const [ genres, setGenres ] = useState([]);
 
+  const getSources = () => {
+    StreamAPI.getSources().then((res) => {
+      setSources(res.data);
+    }).catch((err) => {
+      console.error("Error fetching sources:", err);
+    });
+  };
+
+  const getGenres = () => {
+    StreamAPI.getSources().then((res) => {
+      setGenres(res.data);
+    }).catch((err) => {
+      console.error("Error fetching genres:", err);
+    });
+  };
+
+  useEffect(() => {
+    getSources();
+    getGenres();
+  }, []);
+
   return (
     <StreamContext.Provider value={{ genres, sources }}>
       {children}
     </StreamContext.Provider>
   );
-}
+};
