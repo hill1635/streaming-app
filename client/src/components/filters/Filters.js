595c8d9b97d2ec2906396e2bf3fa068c2a022e7d
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-11-09 13:23:27 -0700

    Added apply filters function.

--- client/src/components/filters/Filters.js
+++ client/src/components/filters/Filters.js
@@ -4,19 +4,25 @@ import './Filters.scss';
 
 function Filters(props) {
   const [ filters, setFilters ] = useState([]);
+  const [ selected, setSelected ] = useState([]);
 
   useEffect(() => {
     setFilters(props.data);
   }, [ props.data ]);
 
+  const applyFilters = async (input) => {
+    var resData = await props.getData(input);
+    props.setData(resData);
+  };
+
   return (
     <div>
       {filters.map((filter) => {
         return (
-          <Filter data={filter} />
+          <Filter data={filter} setSelected={setSelected} selected={selected} />
         );
       })}
-      <button>Apply</button>
+      <button onClick={() => applyFilters(selected)}>Apply</button>
     </div>
   );
 }
