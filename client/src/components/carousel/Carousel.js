e1af98991a99af3fa564ea5d9c9cb9b0f4db16e7
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-11-09 13:22:22 -0700

    Created carousel component.

--- client/src/components/carousel/Carousel.js
+++ client/src/components/carousel/Carousel.js
@@ -1,13 +1,26 @@
-import React, { useContext, useState, useEffect } from 'react';
-import './Carousel.scss';
+import React, { useState, useEffect } from "react";
+import CarouselItem from "./CarouselItem";
+import "./Carousel.scss";
+import Filters from "../filters/Filters";
 
 function Carousel(props) {
+  const [filters, setFilters] = useState([]);
+  const [data, setData] = useState([]);
 
-	return (
+  useEffect(() => {
+    setFilters(props.filters);
+  }, [props.filters]);
+
+  return (
     <section>
-      
+      <Filters data={filters} getData={props.getData} setData={setData} />
+      {data.map((item) => {
+        return (
+          <CarouselItem data={item} />
+        );
+      })}
     </section>
-	);
+  );
 }
 
 export default Carousel;
