44671d22553960bcddedc48ea290e4b42dcd4af5
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-11-10 20:57:25 -0700

    Added route to get title by id.

--- routes/api/stream.js
+++ routes/api/stream.js
@@ -3,6 +3,7 @@ const streamController = require('../../controllers/streamController');
 
 router.route('/sources').get(streamController.getSources);
 router.route('/genres').get(streamController.getGenres);
+router.route('/title/:id').get(streamController.getTitle);
 router.route('/titles').get(streamController.getTitles);
 
 module.exports = router;
