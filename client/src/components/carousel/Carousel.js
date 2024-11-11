7eaa39de200222ea29fcc66eadae341346d98acf
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-11-10 21:01:24 -0700

    Added display filter count to data map.

--- client/src/components/carousel/Carousel.js
+++ client/src/components/carousel/Carousel.js
@@ -6,6 +6,7 @@ import Filters from "../filters/Filters";
 function Carousel(props) {
   const [filters, setFilters] = useState([]);
   const [data, setData] = useState([]);
+  const [display, setDisplay] = useState(0);
 
   useEffect(() => {
     setFilters(props.filters);
@@ -13,12 +14,12 @@ function Carousel(props) {
 
   return (
     <section>
-      <Filters data={filters} getData={props.getData} setData={setData} />
-      {data.map((item) => {
+      <Filters data={filters} getData={props.getDataArray} setData={setData} setDisplay={setDisplay}/>
+        {data.slice(0, display).map((item) => {
         return (
-          <CarouselItem data={item} />
+            <CarouselItem data={item} getData={props.getDataIndex} />
         );
-      })}
+        })}
     </section>
   );
 }
