1d089b4b2c9f33c2d7aa772bc1829a31f722bf65
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-11-10 21:07:40 -0700

    Expanded on component.

--- client/src/components/carousel/CarouselItem.js
+++ client/src/components/carousel/CarouselItem.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import './Carousel.scss';
 
 function CarouselItem(props) {
+  const [ item, setItem ] = useState({});
+
+  const getData = useCallback(async (id) => {
+    const res = await props.getData(id);
+    console.log("res:", res);
+    setItem(res);
+  }, [props.data]);
+
+  useEffect(() => {
+    if (props.data.imdb_id) {
+      getData(props.data.imdb_id);
+    }
+  }, [props.data]);
+
   return (
-    <span>
-      <img src={props.data.image} alt={props.data.title} />
-      <h3>{props.data.title}</h3>
-      <p>{props.data.year}</p>
+    <span className="carouselItem">
+      <div className="itemImg">
+        <img src={item.imgSrc} alt={item.title} />
+      </div>
+      <div className="itemDetails">
+        <h1 className="itemTitle">{item.title}</h1>
+        <h3 className="itemDate">{item.date}</h3> 
+        <p className="itemDescription">{item.description}</p>
+        <button className="itemBtn" onClick={() => getData(props.data.imdb_id)}>More</button>
+      </div>
     </span>
   );
 }
