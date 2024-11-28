09b89fb9f7c521ade86bed9ad2f3bafdcf0eb4d7
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-11-28 13:02:25 -0700

    Added route for networks.

--- routes/api/stream.js
+++ routes/api/stream.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const streamController = require('../../controllers/streamController');
 
 router.route('/sources').get(streamController.getSources);
+router.route('/networks').get(streamController.getNetworks);
 router.route('/genres').get(streamController.getGenres);
 router.route('/title/:id').get(streamController.getTitle);
 router.route('/titles').get(streamController.getTitles);
