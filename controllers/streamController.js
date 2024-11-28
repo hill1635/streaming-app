46eab9bdbdcd1e2dc0cb3db3f53cd4a0412978b2
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-11-28 13:02:04 -0700

    Added controller function to get networks.

--- controllers/streamController.js
+++ controllers/streamController.js
@@ -3,7 +3,17 @@ var qs = require('qs');
 
 module.exports = {
   getSources: function (req, res) {
-    axios.get('https://api.watchmode.com/v1/sources/?types=sub&apiKey=' + process.env.REACT_APP_WATCHMODE_API_KEY)
+    axios.get('https://api.watchmode.com/v1/sources/?apiKey=' + process.env.REACT_APP_WATCHMODE_API_KEY)
+      .then((response) => {
+        res.json(response.data);
+      })
+      .catch((err) => {
+        console.error('Error getting sources:', err);
+        res.status(500).json({ error: 'An error occurred while retrieving the sources' });
+      });
+  },
+  getNetworks: function (req, res) {
+    axios.get('https://api.watchmode.com/v1/networks/?apiKey=' + process.env.REACT_APP_WATCHMODE_API_KEY)
       .then((response) => {
         res.json(response.data);
       })
